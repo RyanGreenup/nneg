081ef6c3b77328c59d05ff1ea77a5799dd0fe610
Ryan G <exogenesis@protonmail.com>
2022-09-10 23:25:19 +1000

    got something working

--- regression_from_scratch.py
+++ regression_from_scratch.py
@@ -1,21 +1,15 @@
-import torch.nn as nn
-import torch
 import numpy as np
-
 import matplotlib.pyplot as plt
 
 N = 10
 x = np.linspace(start=1, stop=10, num=N)
 e = np.random.normal(0, 10, N)
-y = x# *x + e
-x = torch.from_numpy(x).reshape(-1, 1).float()
-y = torch.from_numpy(y).reshape(-1, 1).float()
+y = x + e
 
-h1 = 1
+h1 = 5
 h2 = 3
-w1: torch.Tensor = torch.tensor(np.random.normal(0, 1, (1, h1)), requires_grad=True)
-w2: torch.Tensor = torch.tensor(np.random.normal(0, 1, (h1, h2)), requires_grad=True)
-w3: torch.Tensor = torch.tensor(np.random.normal(0, 1, (h2, 1)), requires_grad=True)
+
+w1 = np.random.normal(0, 1, (1, h1))
 
 def model(x):
     x = torch.matmul(x, w1.float())
@@ -25,33 +19,34 @@ def model(x):
     # x = torch.matmul(x, w3.float())
     return x
 
-plt.scatter(x, y )
-yhat = model(x).detach().numpy()
-plt.plot(x, yhat, color="red", linewidth = 9)
-plt.show()
+def loss(x):
+    return np.mean((x-model(x))**2)
 
-eta = 1e-6
+eta = 1e-5
+
+
 
 losses = []
-EPOCH = 10000
+EPOCH = 100000
 print("|------------------------------------------------------------------------------|")
 print(" ", end='', flush=True)
 for t in range(EPOCH):
-# Forward pass
+    # Forward pass
     yhat = model(x.float())
 
-# Measure the loss
-    loss: torch.Tensor = torch.sum((yhat - y)**2)
+    # Measure the loss
+    loss: torch.Tensor = loss_fn(y, model(x))
     loss.backward()
     losses.append(loss.detach().numpy())
 
-# Backprop
-# for p in [w1, w2, w3]:
 
-    with torch.no_grad():
-        temp = w1 - w1.grad * eta
-        w1.copy_(temp)
-        w1.grad = None
+
+    # Backprop
+    for p in [w1, w2, w3]:
+        with torch.no_grad():
+            temp = p - p.grad * eta
+            p.copy_(temp)
+            p.grad = None
 
     if t in np.floor(np.linspace(start=0, stop=EPOCH, num=78)):
         print("#", end='', flush=True)
@@ -65,6 +60,3 @@ plt.scatter(x, y )
 yhat = model(x).detach().numpy()
 plt.plot(x, yhat, color="red", linewidth = 9)
 plt.show()
-
-
-
