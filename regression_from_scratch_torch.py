38bba61a298376de388b7ae9db588fcd43357382
Ryan G <exogenesis@protonmail.com>
2022-09-10 19:25:16 +1000

    still working on it

--- regression_from_scratch_torch.py
+++ regression_from_scratch_torch.py
@@ -4,45 +4,35 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-N = 50
+N = 10
 x = np.linspace(start=1, stop=10, num=N)
 e = np.random.normal(0, 10, N)
 y = x*x + e
 x = torch.from_numpy(x).reshape(-1, 1).float()
 y = torch.from_numpy(y).reshape(-1, 1).float()
 
-h1 = 2**5
-h2 = 1
-w1 = torch.from_numpy(np.random.normal(h1, N)).reshape(-1, 1).float()
-w1.shape
-3+3
+h1 = 5
+h2 = 3
+w1: torch.Tensor = torch.tensor(np.random.normal(0, 1, (1, h1)), requires_grad=True)
+w2: torch.Tensor = torch.tensor(np.random.normal(0, 1, (h1, h2)), requires_grad=True)
+w3: torch.Tensor = torch.tensor(np.random.normal(0, 1, (h2, 1)), requires_grad=True)
 
-w1 = torch.from_numpy(np.random.normal(h2)).reshape(-1, 1).float()
-def model(x, y):
-    out =
+def model(x):
+    x = torch.matmul(x, w1.float())
+    x = torch.sigmoid(x)
+    x = torch.matmul(x, w2.float())
+    x = torch.sigmoid(x)
+    x = torch.matmul(x, w3.float())
+    return x
 
 
-## Define a model
-model = nn.Sequential(
-    nn.Linear(in_features=1, out_features=h1),
-)
-model.append(
-        nn.Sequential(
-            nn.Sigmoid(),
-            nn.Linear(h1, h2)))
-#model.append(
-#        nn.Sequential(
-#            nn.Sigmoid(),
-#            nn.Linear(h2, 1)))
-
+eta = 1e-5
 loss_fn = nn.MSELoss()
-eta = 1e-2
-opt = torch.optim.SGD(model.parameters(), lr = eta, momentum=0)
 
 
 
 losses = []
-EPOCH = 1000
+EPOCH = 100000
 print("|------------------------------------------------------------------------------|")
 print(" ", end='', flush=True)
 for t in range(EPOCH):
@@ -50,16 +40,24 @@ for t in range(EPOCH):
     yhat = model(x.float())
 
     # Measure the loss
-    loss = loss_fn(yhat, y)
+    loss: torch.Tensor = loss_fn(y, model(x))
+    loss.backward()
+    losses.append(loss.detach().numpy())
+
+
+
+    # Backprop
+    for p in [w1, w2, w3]:
+        with torch.no_grad():
+            temp = p - p.grad * eta
+            p.copy_(temp)
 
     if t in np.floor(np.linspace(start=0, stop=EPOCH, num=78)):
         print("#", end='', flush=True)
 
 
-    # Backprop
-    opt.zero_grad()
-    loss.backward()
-    opt.step()
+plt.plot(losses)
+plt.show()
 
 
 plt.scatter(x, y )
