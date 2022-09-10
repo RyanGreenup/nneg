d81d5b9af0be7dd1519812a98a5fcf1a537a63f2
Ryan G <exogenesis@protonmail.com>
2022-09-10 23:46:16 +1000

    torch for regression

--- regression_from_scratch_torch.py
+++ regression_from_scratch_torch.py
@@ -1,28 +1,31 @@
+# Imports
 import torch.nn as nn
 import torch
 import numpy as np
-
 import matplotlib.pyplot as plt
 
-N = 10
+# Create Data
+N = 30
 x = np.linspace(start=1, stop=10, num=N)
 e = np.random.normal(0, 10, N)
-y = x# *x + e
+y = x *x + e
 x = torch.from_numpy(x).reshape(-1, 1).float()
 y = torch.from_numpy(y).reshape(-1, 1).float()
 
-h1 = 1
-h2 = 3
+# Generate Weights
+h1 = 32
+h2 = 16
 w1: torch.Tensor = torch.tensor(np.random.normal(0, 1, (1, h1)), requires_grad=True)
 w2: torch.Tensor = torch.tensor(np.random.normal(0, 1, (h1, h2)), requires_grad=True)
 w3: torch.Tensor = torch.tensor(np.random.normal(0, 1, (h2, 1)), requires_grad=True)
 
+
 def model(x):
     x = torch.matmul(x, w1.float())
-    # x = torch.sigmoid(x)
-    # x = torch.matmul(x, w2.float())
-    # x = torch.sigmoid(x)
-    # x = torch.matmul(x, w3.float())
+    x = torch.sigmoid(x)
+    x = torch.matmul(x, w2.float())
+    x = torch.sigmoid(x)
+    x = torch.matmul(x, w3.float())
     return x
 
 plt.scatter(x, y )
@@ -30,8 +33,7 @@ yhat = model(x).detach().numpy()
 plt.plot(x, yhat, color="red", linewidth = 9)
 plt.show()
 
-eta = 1e-6
-
+eta = 5e-6
 losses = []
 EPOCH = 10000
 print("|------------------------------------------------------------------------------|")
@@ -45,13 +47,11 @@ for t in range(EPOCH):
     loss.backward()
     losses.append(loss.detach().numpy())
 
-# Backprop
-# for p in [w1, w2, w3]:
-
-    with torch.no_grad():
-        temp = w1 - w1.grad * eta
-        w1.copy_(temp)
-        w1.grad = None
+    for p in [w1, w2, w3]:
+        with torch.no_grad():
+            temp = p - p.grad * eta
+            p.copy_(temp)
+            p.grad = None
 
     if t in np.floor(np.linspace(start=0, stop=EPOCH, num=78)):
         print("#", end='', flush=True)
