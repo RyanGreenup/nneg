a9b93a14cdb61cb7d5f7731aec5f71feb92a007b
Ryan G <exogenesis@protonmail.com>
2022-09-12 16:38:34 +1000

    linear working example

--- regression_from_scratch.py
+++ regression_from_scratch.py
@@ -8,13 +8,13 @@ import matplotlib.pyplot as plt
 N = 30
 x = np.linspace(start=1, stop=10, num=N)
 e = np.random.normal(0, 10, N)
-y = x *x + e
+y = x
 
 x = x.reshape(-1,1)
 y = y.reshape(-1, 1)
 
 # Generate Weights
-h1 = 32
+h1 = 1
 h2 = 16
 w1: np.ndarray = np.random.normal(0, 1, (1, h1))
 w2: np.ndarray = np.random.normal(0, 1, (h1, h2))
@@ -24,7 +24,7 @@ w2_grad = [0]
 w3_grad = [0]
 
 def matmul(x: np.ndarray, w: np.ndarray):
-    return np.matmul(x, w), x.transpose()
+    return np.matmul(x, w), x
 
 def sigmoid(x):
     grad = -np.exp(x)/(np.exp(x) + 1)**2
@@ -35,54 +35,36 @@ def model(x):
     grad = 1
     x, dw = matmul(x, w1)
     grad *= dw
-    x, dw = sigmoid(x)
-    grad = np.matmul(grad, dw)
-    w1_grad = grad
-    # Second Layer
-    grad = 1
-    x, dw = matmul(x, w2)
-    grad *= dw
-    x, dw = sigmoid(x)
-    grad = np.matmul(grad, dw)
-    w2_grad = grad
-    # Third Layer
-    grad = 1
-    x, dw = matmul(x, w3)
-    grad *= dw
-    w3_grad = grad
-    return x, [w1_grad, w2_grad, w3_grad]
+    return x, grad
 
 plt.scatter(x, y )
-yhat = model(x)[0]
+yhat, _ = model(x)
 plt.plot(x, yhat, color="red", linewidth = 9)
 plt.show()
 
 
-def my_loss(x):
-    l = np.sum((model(x)-x)**2)
-    g = 2*np.sum((model(x)-x))
+def my_loss(x, y):
+    yhat, _ = model(x)
+    l = np.sum((yhat-y)**2)
+    g = 2*(yhat-y)
     return l, g
 
 eta = 5e-6
 losses = []
-EPOCH = 10000
+EPOCH = 100000
+
 print("|------------------------------------------------------------------------------|")
 print(" ", end='', flush=True)
 for t in range(EPOCH):
-# Forward pass
-    yhat, grad = model(x)
-
-# Measure the loss
-    loss, dy = my_loss(x)
-    # loss.backward()
-    grad *= dy
-    losses.append(loss)
-
-    # Backprop
-    w1 -= grad[0]
-    w2 -= grad[1]
-    w3 -= grad[2]
-
+# Forward Pass
+    yhat, dw = model(x)
+# Loss
+    l, dy    = my_loss(x, y)
+    losses.append(l)
+# Backward
+    grad = np.matmul(dy.transpose(), dw)
+# Backprop
+    w1 -= grad * eta
     if t in np.floor(np.linspace(start=0, stop=EPOCH, num=78)):
         print("#", end='', flush=True)
 
@@ -92,7 +74,7 @@ plt.show()
 
 
 plt.scatter(x, y )
-yhat = model(x)
+yhat, _ = model(x)
 plt.plot(x, yhat, color="red", linewidth = 9)
 plt.show()
 
